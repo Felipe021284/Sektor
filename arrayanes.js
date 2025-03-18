e7bc9a2ee436a55f493a1e17bd8bda47101221f1
Felipe Martínez <105995332+Felipe021284@users.noreply.github.com>
2025-03-18 16:18:15 -0600

    Add files via upload

---
+++ arrayanes.js
@@ -0,0 +1,53 @@
+/*creado por Felipe Jiménez*/
+      window.addEventListener("load", function () {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("box").style.display = "block";
+      });
+      const testimonials = [
+        {
+  
+          testimonial:
+             "<a href='https://felipe021284.github.io/Sektor/ARRAYANES 1.HTML'>GRUPO 1</a>",
+        },
+        {
+          
+          
+          testimonial:
+           "<a href='https://felipe021284.github.io/Sektor/ARRAYANES 2.HTML'>GRUPO 2</a>",
+        },
+        
+        {
+          
+          
+          testimonial:
+            "<a href='https://felipe021284.github.io/Sektor/ARRAYANES 3.HTML'>GRUPO 3</a>",
+        },
+        
+         
+     
+        
+       
+       
+      ];
+      let i = 0; // current slide
+      let j = testimonials.length; // total slides
+      let testimonialContainer = document.getElementById(
+        "testimonial-container"
+      );
+      function next() {
+        i = (j + i + 1) % j;
+        displayTestimonial();
+      }
+
+      function prev() {
+        i = (j + i - 1) % j;
+        displayTestimonial();
+      }
+      let displayTestimonial = () => {
+        testimonialContainer.innerHTML = `
+        <p>${testimonials[i].testimonial}</p>
+      <!--img src=${testimonials[i].image}></img-->
+        
+        `;
+      };
+      window.onload = displayTestimonial;
