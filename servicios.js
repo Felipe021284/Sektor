ddd9c9f71cd7fcb64cc46e456bc3fae93de197e9
Felipe Martínez <105995332+Felipe021284@users.noreply.github.com>
2025-04-12 02:43:36 -0600

    Actualizar servicios.js

--- servicios.js
+++ servicios.js
@@ -1,50 +1,39 @@
-window.addEventListener("load", function () {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("box").style.display = "block";
-      });
-      const testimonials = [
-        {
-  
-          testimonial:
-             "<a href='https://felipe021284.github.io/Sektor/PROLOGIS.HTML'>PROLOGIS</a>",
-        },
-        {
-          
-          
-          testimonial:
-           "<a href='https://felipe021284.github.io/Sektor/LA%20CONCHA.HTML'>LA CONCHA</a>",
-        },
-        {
-          
-          
-          testimonial:
-            "<a href='https://felipe021284.github.io/Sektor/SUNNY.HTML'>SUNNY</a>",
-        },
-        
-      
-        
-       
-       
-      ];
-      let i = 0; // current slide
-      let j = testimonials.length; // total slides
-      let testimonialContainer = document.getElementById(
-        "testimonial-container"
-      );
-      function next() {
-        i = (j + i + 1) % j;
-        displayTestimonial();
-      }
+const slider = document.querySelector('.slider');
+const sliderItems = document.querySelectorAll('.slider-item');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
 
-      function prev() {
-        i = (j + i - 1) % j;
-        displayTestimonial();
-      }
-      let displayTestimonial = () => {
-        testimonialContainer.innerHTML = `
-        <p>${testimonials[i].testimonial}</p>
-      <!--img src=${testimonials[i].image}></img-->
-        
-        `;
-      };
-      window.onload = displayTestimonial;
+let currentIndex = 0;
+let isDragging = false;
+let startX = 0;
+
+slider.addEventListener('touchstart', (e) => {
+  isDragging = true;
+  startX = e.touches[0].clientX;
+});
+
+slider.addEventListener('touchmove', (e) => {
+  if (isDragging) {
+    const currentX = e.touches[0].clientX;
+    const diffX = currentX - startX;
+    slider.style.transform = `translateX(${diffX}px)`;
+  }
+});
+
+slider.addEventListener('touchend', (e) => {
+  isDragging = false;
+  const currentX = e.changedTouches[0].clientX;
+  const diffX = currentX - startX;
+  if (diffX < -100) {
+    currentIndex++;
+    if (currentIndex >= sliderItems.length) {
+      currentIndex = 0;
+    }
+  } else if (diffX > 100) {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = sliderItems.length - 1;
+    }
+  }
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+});
